61394025f138ed586b26536b061533728f0624e0
mong'are <mongaresams@gmail.com>
2022-05-14 17:41:45 +0300

    add static view to the app

--- app/controllers/v1/greetings_controller.rb
+++ app/controllers/v1/greetings_controller.rb
@@ -1,9 +1,9 @@
 class V1::GreetingsController < ApplicationController
   def index
+    @greetings =   Greeting.all.sample(1)
     render json: { :greeting => [
       {
-        :name => 'Hello Sam',
-        :message => 'Welcome to the API'
+        message: @greetings[0].greeting
       }
     ] }.to_json
   end
